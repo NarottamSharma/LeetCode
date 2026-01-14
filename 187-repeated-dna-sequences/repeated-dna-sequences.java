a617fbd5f45ca5348531b280160e36595be22669
Narottam Sharma <narottamaiengineer@gmail.com>
2026-01-14 11:50:26 +0530

    Time: 19 ms (55.06%) | Memory: 56.1 MB (9.44%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,5 +1,25 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
+        if (s.length() < 10) {
+            return new ArrayList<>();
+        }
+        Set<String> seen = new HashSet<>(); // renamed for clarity
+        Set<String> repeated = new HashSet<>();
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String sub = s.substring(i, i + 10);
+            if (seen.contains(sub)) {
+                repeated.add(sub);
+            } else {
+                seen.add(sub);
+            }
+        }
+        return new ArrayList<>(repeated);
+    }
+
+}
+
+/*
+public List<String> findRepeatedDnaSequences(String s) {
         // Use a Set to store sequences we have seen so far
         Set<String> seen = new HashSet<>();
         
@@ -22,4 +42,4 @@ class Solution {
         // Convert the result Set to a List
         return new ArrayList<>(repeated);
     }
-}
+ */
