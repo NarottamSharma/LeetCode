f576df92f15f1ede2933f6657976ec16cb4a62f2
Narottam Sharma <narottamaiengineer@gmail.com>
2026-01-17 16:36:40 +0530

    Time: 1 ms (98.03%) | Memory: 47.5 MB (46.21%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,18 +1,29 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int left =0;
-        int totalSum=0;
-        for(int i=0;i<nums.length;i++){
-            totalSum+=nums[i];
+        // 1. Calculate the Total Sum of the entire array
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
         }
-        for(int i=0;i<nums.length;i++){
-            int right = totalSum-left-nums[i];
-            if(left==right){
-                return i;
+
+        // 2. Initialize leftSum
+        int leftSum = 0;
+
+        // 3. Iterate through the array to find the pivot
+        for (int i = 0; i < nums.length; i++) {
+            // We calculate rightSum on the fly using the formula
+            int rightSum = totalSum - leftSum - nums[i];
+
+            // 4. Check if we found the pivot
+            if (leftSum == rightSum) {
+                return i; // Found it! Return the index immediately
             }
-            left+=nums[i];
+
+            // 5. If not, add the current number to leftSum for the next iteration
+            leftSum += nums[i];
         }
+
+        // 6. If we finish the loop without returning, no pivot exists
         return -1;
-            
     }
 }
