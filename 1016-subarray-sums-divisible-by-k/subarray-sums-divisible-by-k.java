c73c02ca689a23362c12403485025e993e01f899
Narottam Sharma <narottamaiengineer@gmail.com>
2026-01-19 15:54:52 +0530

    Time: 25 ms (51.22%) | Memory: 51.5 MB (67.12%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,20 +1,36 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int count=0;
-        int currentSum=0;
-        map.put(0,1);
-        for(int num:nums){
-            currentSum+=num;
-            int rem = currentSum%k;
-            if(rem<0){
-                rem+=k;
+        // Map stores <Remainder, Frequency>
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // Base case: A remainder of 0 exists before the array starts
+        map.put(0, 1);
+        
+        int currentSum = 0;
+        int count = 0;
+        
+        for (int num : nums) {
+            currentSum += num;
+            
+            // Calculate Remainder
+            int rem = currentSum % k;
+            
+            // JAVA TRICK: Handle negative remainders!
+            // If rem is -2 and k is 5, we want the remainder to be 3.
+            if (rem < 0) {
+                rem += k;
             }
-            if(map.containsKey(rem)){
-                count+=map.get(rem);
+            
+            // If we have seen this remainder before, it means the 
+            // chunk between that previous point and now is divisible by k.
+            if (map.containsKey(rem)) {
+                count += map.get(rem);
             }
-            map.put(rem,map.getOrDefault(rem,0)+1);
+            
+            // Store the current remainder
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
         }
+        
         return count;
     }
 }
