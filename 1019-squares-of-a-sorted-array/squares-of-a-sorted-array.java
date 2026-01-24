0766edc6d94ff6a0576eb7e2866c51bf86dd9335
Narottam Sharma <narottamaiengineer@gmail.com>
2026-01-24 13:24:57 +0530

    Time: 1 ms (99.85%) | Memory: 47.1 MB (82.33%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,36 +1,25 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-
-        int n = nums.length;              // Length of input array
-
-        int right = n - 1;                // Right pointer at the last index
-        int left = 0;                     // Left pointer at the first index
-        int pos = n - 1;                  // Position to fill in result from the end
-
-        int[] res = new int[n];           // Result array to store sorted squares
-
-        // Loop until the two pointers cross
-        while (left <= right) {
-
-            // Square of the element at the right pointer
-            int rightSq = nums[right] * nums[right];
-
-            // Square of the element at the left pointer
-            int leftSq = nums[left] * nums[left];
-
-            // The larger square should go at the current 'pos' index
-            if (rightSq < leftSq) {
-                res[pos] = leftSq;        // Place left square at current position
-                pos--;                    // Move position backward
-                left++;                   // Move left pointer to the right
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        int left = 0;
+        int right = n - 1;
+        
+        // We fill the result array from right to left (largest to smallest)
+        for (int i = n - 1; i >= 0; i--) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            
+            if (leftSquare > rightSquare) {
+                result[i] = leftSquare;
+                left++; // We used the left number, so move left pointer forward
             } else {
-                res[pos] = rightSq;       // Place right square at current position
-                pos--;                    // Move position backward
-                right--;                  // Move right pointer to the left
+                result[i] = rightSquare;
+                right--; // We used the right number, so move right pointer backward
             }
         }
-
-        // At this point, 'res' contains all squares in non-decreasing order
-        return res;
+        
+        return result;
     }
 }
