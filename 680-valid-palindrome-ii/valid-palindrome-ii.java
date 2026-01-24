94a56fdfd6740ae1a660c876bcb3e4bf1507c114
Narottam Sharma <narottamaiengineer@gmail.com>
2026-01-24 13:36:57 +0530

    Time: 4 ms (99.59%) | Memory: 47.7 MB (76.94%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,30 +1,34 @@
 class Solution {
-    public boolean palindromeHelper(int i,int j,String s){
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            // Match found, continue shrinking the window
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } 
+            // Mismatch found! This is our "One Strike".
+            else {
+                // Try deleting left char OR deleting right char
+                // If either results in a palindrome, we win.
+                return check(s, left + 1, right) || check(s, left, right - 1);
+            }
+        }
+        
+        return true; // No mismatches ever found
+    }
+
+    // Standard palindrome checker for a substring range
+    private boolean check(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
             i++;
             j--;
         }
         return true;
-    }
-    public boolean validPalindrome(String s) {
-        int i=0;
-        int j=s.length()-1;
-        while(i<j){
-            char left =s.charAt(i),
-                right = s.charAt(j);
-            
-            if(left!=right){
-                return palindromeHelper(i+1,j,s)|| palindromeHelper(i,j-1,s);
-            } else{
-                i++;
-                j--;
-            }
-
-        }
-        return true;
-    
     }
 }
