0f60d1a7c57933b72375c052f77d1340beb78160
Narottam Sharma <narottamaiengineer@gmail.com>
2026-02-04 12:05:38 +0530

    Time: 2 ms (38.16%) | Memory: 43.1 MB (27.57%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,20 +1,18 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
         List<Integer> list = new ArrayList<>();
-        for(int stone:stones){
+        for (int stone : stones) {
             list.add(stone);
         }
-        while(list.size()>1){
-            Collections.sort(list,Collections.reverseOrder());
-
-            int stone1 = list.remove(0);
-            int stone2 = list.remove(0);
-            int addStone = stone1- stone2;
-            if(addStone>0){
+        while (list.size() > 1) {
+            Collections.sort(list, Collections.reverseOrder());  // Sort descending for easier access
+            int stone1 = list.remove(0);  // Heaviest
+            int stone2 = list.remove(0);  // Next heaviest
+            int addStone = stone1 - stone2;
+            if (addStone > 0) {  // Only add if difference > 0
                 list.add(addStone);
             }
         }
-        return list.isEmpty()?0:list.get(0);
-
+        return list.isEmpty() ? 0 : list.get(0);
     }
 }
