4afaf2243c3fcc42426f1adc6066e9753986b302
Narottam Sharma <narottamaiengineer@gmail.com>
2026-02-04 12:49:57 +0530

    Time: 3 ms (50.35%) | Memory: 42.3 MB (72.52%) - LeetSync

--- 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.java
+++ 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.java
@@ -1,29 +1,37 @@
+import java.util.*;
+
 class Solution {
     public int largestInteger(int num) {
-        PriorityQueue<Integer> even = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> odd = new PriorityQueue<>(Collections.reverseOrder());
+        // 1. Setup two Max-Heaps
+        PriorityQueue<Integer> evens = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> odds = new PriorityQueue<>(Collections.reverseOrder());
+        
+        // Convert number to string to easily iterate digits
         String s = String.valueOf(num);
         int[] digits = new int[s.length()];
-
-        for(int i=0;i<s.length();i++){
-            int digit = s.charAt(i)-'0';
-            digits[i] = digit;
-            if(digit%2==0){
-                even.offer(digit);
-            } else{
-                odd.add(digit);
+        
+        // 2. Populate Heaps
+        for (int i = 0; i < s.length(); i++) {
+            int digit = s.charAt(i) - '0';
+            digits[i] = digit; // Store original digit to remember parity later
+            if (digit % 2 == 0) {
+                evens.add(digit);
+            } else {
+                odds.add(digit);
             }
         }
+        
+        // 3. Rebuild the number
         StringBuilder res = new StringBuilder();
-        for(int digit:digits){
-            if(digit%2==0){
-                res.append(even.poll());
-            } else{
-                res.append(odd.poll());
+        for (int digit : digits) {
+            // Check the parity of the ORIGINAL digit at this position
+            if (digit % 2 == 0) {
+                res.append(evens.poll()); // Pick largest available Even
+            } else {
+                res.append(odds.poll());  // Pick largest available Odd
             }
         }
-
+        
         return Integer.parseInt(res.toString());
-
     }
 }
