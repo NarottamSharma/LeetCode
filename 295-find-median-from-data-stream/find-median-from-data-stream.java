b764b5b41a4b1bb7267b12cd46c379e25999ab86
Narottam Sharma <narottamaiengineer@gmail.com>
2026-02-05 13:31:07 +0530

    Time: 133 ms (67.17%) | Memory: 111.5 MB (69.39%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,25 +1,44 @@
+import java.util.*;
+
 class MedianFinder {
-    PriorityQueue<Integer> rightHeap;
-    PriorityQueue<Integer> leftHeap;
+    // Left side (Max Heap) stores the smaller half
+    private PriorityQueue<Integer> leftHeap;
+    // Right side (Min Heap) stores the larger half
+    private PriorityQueue<Integer> rightHeap;
 
     public MedianFinder() {
+        // Init heaps
         leftHeap = new PriorityQueue<>(Collections.reverseOrder());
-        rightHeap =new PriorityQueue<>();
+        rightHeap = new PriorityQueue<>();
     }
     
     public void addNum(int num) {
-        leftHeap.add(num);
-        rightHeap.add(leftHeap.poll());
-        if(leftHeap.size()<rightHeap.size()){
+        // Optimization: Decide strictly where to put it first
+        if (leftHeap.isEmpty() || num <= leftHeap.peek()) {
+            leftHeap.add(num);
+        } else {
+            rightHeap.add(num);
+        }
+
+        // Rebalance Step: Ensure size difference is never > 1
+        
+        // If Left is too heavy (more than 1 element bigger than Right)
+        if (leftHeap.size() > rightHeap.size() + 1) {
+            rightHeap.add(leftHeap.poll());
+        }
+        // If Right is too heavy (Right should never be bigger than Left in my design)
+        else if (rightHeap.size() > leftHeap.size()) {
             leftHeap.add(rightHeap.poll());
         }
     }
     
     public double findMedian() {
-        if(leftHeap.size()>rightHeap.size()){
+        // If Left has more elements, it holds the median
+        if (leftHeap.size() > rightHeap.size()) {
             return leftHeap.peek();
         }
-        return (leftHeap.peek()+rightHeap.peek())/2.0;
+        // Otherwise, it's the average of the two peaks
+        return (leftHeap.peek() + rightHeap.peek()) / 2.0;
     }
 }
 
