a56b43556f0ee08a1116d019902c4f9c1e9e16bc
Narottam Sharma <narottamaiengineer@gmail.com>
2026-02-06 11:56:21 +0530

    Time: 33 ms (95.31%) | Memory: 51.3 MB (85.16%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,5 +1,56 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
+        int targetArrival = times[targetFriend][0];
+        
+        // 1. Sort the times array directly (or a copy) by Arrival Time
+        // We don't need to add an index column anymore.
+        Arrays.sort(times, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        // 2. Heaps
+        // Occupied: {leaveTime, chairIndex}
+        PriorityQueue<int[]> occupied = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        // Available: {chairIndex}
+        PriorityQueue<Integer> available = new PriorityQueue<>();
+        
+        int nextNewChair = 0;
+        
+        // 3. Traditional For Loop
+        int n = times.length;
+        for (int i = 0; i < n; i++) {
+            int arrival = times[i][0];
+            int leaving = times[i][1];
+            
+            // Step A: Release chairs (Same logic)
+            while (!occupied.isEmpty() && occupied.peek()[0] <= arrival) {
+                available.add(occupied.poll()[1]);
+            }
+            
+            // Step B: Assign chair (Same logic)
+            int currentChair;
+            if (available.isEmpty()) {
+                currentChair = nextNewChair++;
+            } else {
+                currentChair = available.poll();
+            }
+            
+            // Step C: OPTIMIZED CHECK
+            // We identify the target solely by their unique arrival time
+            if (arrival == targetArrival) {
+                return currentChair;
+            }
+            
+            // Step D: Record occupancy
+            occupied.add(new int[]{leaving, currentChair});
+        }
+        
+        return -1;
+
+    }
+}
+
+/**
+
+public int smallestChair(int[][] times, int targetFriend) {
         // sort times by start time
         // we need n chair
         // linear data structure , stores end time of chair
@@ -24,4 +75,5 @@ class Solution {
         return -1;
 
     }
-}
+
+ */
